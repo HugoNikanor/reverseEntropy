23d0eb86cd5c4648b12613b33d295b6fa62af514
Hugo <hugo.hornquist@gmail.com>
2015-08-17 09:30:04 +0200

    better str comp

--- reverseEntropy.cpp
+++ reverseEntropy.cpp
@@ -7,15 +7,10 @@
 
 int main(int argc, char* argv[]) {
 	if(argc == 4) {
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < strlen(argv[i]); j++) {
-				argv[i][j] = toupper(argv[i][j]);
-			}
-		}
 		if(
-			strcmp(argv[1], "ENTROPY")  == 0 &&
-			strcmp(argv[2], "BE")       == 0 &&
-			strcmp(argv[3], "REVERSED") == 0 
+			strcasecmp(argv[1], "ENTROPY")  == 0 &&
+			strcasecmp(argv[2], "BE")       == 0 &&
+			strcasecmp(argv[3], "REVERSED") == 0 
 		) {
 			std::cout <<
 			"THERE IS AS YET INSUFFICIENT DATA FOR A MEANINGFUL ANSWER"
